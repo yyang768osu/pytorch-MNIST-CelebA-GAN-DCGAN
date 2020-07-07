61e0edc2f1401e38da8e4cd55dc71b03bc3d6b6c
yyang768osu <yyang.ohiostate@gmail.com>
2020-07-06 17:45:53 -0700

    Update pytorch_MNIST_DCGAN.py

--- pytorch_MNIST_DCGAN.py
+++ pytorch_MNIST_DCGAN.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.autograd import Variable
 
 # G(z)
 class generator(nn.Module):
@@ -37,7 +36,7 @@ class generator(nn.Module):
         x = F.relu(self.deconv2_bn(self.deconv2(x)))
         x = F.relu(self.deconv3_bn(self.deconv3(x)))
         x = F.relu(self.deconv4_bn(self.deconv4(x)))
-        x = F.tanh(self.deconv5(x))
+        x = torch.tanh(self.deconv5(x))
 
         return x
 
@@ -65,7 +64,7 @@ class discriminator(nn.Module):
         x = F.leaky_relu(self.conv2_bn(self.conv2(x)), 0.2)
         x = F.leaky_relu(self.conv3_bn(self.conv3(x)), 0.2)
         x = F.leaky_relu(self.conv4_bn(self.conv4(x)), 0.2)
-        x = F.sigmoid(self.conv5(x))
+        x = torch.sigmoid(self.conv5(x))
 
         return x
 
@@ -75,10 +74,12 @@ def normal_init(m, mean, std):
         m.bias.data.zero_()
 
 fixed_z_ = torch.randn((5 * 5, 100)).view(-1, 100, 1, 1)    # fixed noise
-fixed_z_ = Variable(fixed_z_.cuda(), volatile=True)
+fixed_z_ = fixed_z_.cuda()
+fixed_z_.requires_grad = False
 def show_result(num_epoch, show = False, save = False, path = 'result.png', isFix=False):
     z_ = torch.randn((5*5, 100)).view(-1, 100, 1, 1)
-    z_ = Variable(z_.cuda(), volatile=True)
+    z_ = z_.cuda()
+    z_.requires_grad = True
 
     G.eval()
     if isFix:
@@ -140,9 +141,9 @@ train_epoch = 20
 # data_loader
 img_size = 64
 transform = transforms.Compose([
-        transforms.Scale(img_size),
+    transforms.Resize(img_size),
         transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+        transforms.Normalize(mean=[0.5], std=[0.5])
 ])
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('data', train=True, download=True, transform=transform),
@@ -193,12 +194,12 @@ for epoch in range(train_epoch):
         y_real_ = torch.ones(mini_batch)
         y_fake_ = torch.zeros(mini_batch)
 
-        x_, y_real_, y_fake_ = Variable(x_.cuda()), Variable(y_real_.cuda()), Variable(y_fake_.cuda())
+        x_, y_real_, y_fake_ = x_.cuda(), y_real_.cuda(), y_fake_.cuda()
         D_result = D(x_).squeeze()
         D_real_loss = BCE_loss(D_result, y_real_)
 
         z_ = torch.randn((mini_batch, 100)).view(-1, 100, 1, 1)
-        z_ = Variable(z_.cuda())
+        z_ = z_.cuda()
         G_result = G(z_)
 
         D_result = D(G_result).squeeze()
@@ -211,13 +212,14 @@ for epoch in range(train_epoch):
         D_optimizer.step()
 
         # D_losses.append(D_train_loss.data[0])
-        D_losses.append(D_train_loss.data[0])
+        # D_losses.append(D_train_loss.data[0])
+        D_losses.append(D_train_loss.item)
 
         # train generator G
         G.zero_grad()
 
         z_ = torch.randn((mini_batch, 100)).view(-1, 100, 1, 1)
-        z_ = Variable(z_.cuda())
+        z_ = z_.cuda()
 
         G_result = G(z_)
         D_result = D(G_result).squeeze()
@@ -225,9 +227,11 @@ for epoch in range(train_epoch):
         G_train_loss.backward()
         G_optimizer.step()
 
-        G_losses.append(G_train_loss.data[0])
+        # G_losses.append(G_train_loss.data[0])
+        G_losses.append(G_train_loss.item)
 
         num_iter += 1
+        print(f"epoch {epoch}/{train_epoch} iter {num_iter}/{len(train_loader)}")
 
     epoch_end_time = time.time()
     per_epoch_ptime = epoch_end_time - epoch_start_time
